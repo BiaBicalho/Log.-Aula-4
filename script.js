f829f6fa230cbfa5db6febfc5ea6c6501c690f30
Bia <85128774+BiaBicalho@users.noreply.github.com>
2022-01-21 20:35:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//1 crie uma funcao que exiba uma mensagem no console
+function frase() {
+  console.log("Olá, bem vindx ao console");
+}
+frase();
+
+//2-crie uma funcao que receba seu nome como parâmetro e exiba no console
+/*function firstName(nome) {
+  console.log(`Olá ${nome}`);
+}
+firstName("Beatriz");*/
+//3- Crie uma função que receba nome,idade e um estilo musical preferido(parâmetro) e exiba no console
+/*function dados(nome, idade, estilo) {
+  console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos e meu estilo musical favorito é ${estilo}.`);
+}
+dados("Bia",18,"acústico");*/
+//4- Crie uma função que receba um filme e uma música (parâmetros) e exiba no console
+/*function arte(filme, musica) {
+  console.log(`Um dos meus filmes favoritos é ${filme} enquanto uma das minhas músicas favoritas é ${musica}`);
+}
+arte("Soul","Happiest Year");*/
